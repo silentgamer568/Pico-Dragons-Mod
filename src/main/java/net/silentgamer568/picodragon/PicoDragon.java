a27b3f22362bd0fcecce045f91560b5176526419
silentgamer568 <boisilent9@gmail.com>
2022-01-31 15:20:27 +0700

    Changed silentg to silentgamer568 to clear confusion

--- src/main/java/net/silentgamer568/picodragon/PicoDragon.java
+++ src/main/java/net/silentgamer568/picodragon/PicoDragon.java
@@ -1,6 +1,7 @@
-package net.silentg.picodragon;
+package net.silentgamer568.picodragon;
 
 import net.fabricmc.api.ModInitializer;
+import net.silentgamer568.picodragon.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,14 +9,12 @@ public class PicoDragon implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final String MOD_ID = "picodragon";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
--- src/main/java/net/silentgamer568/picodragon/mixin/ExampleMixin.java
+++ src/main/java/net/silentgamer568/picodragon/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.silentg.picodragon.mixin;
+package net.silentgamer568.picodragon.mixin;
 
-import net.silentg.picodragon.PicoDragon;
+import net.silentgamer568.picodragon.PicoDragon;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
